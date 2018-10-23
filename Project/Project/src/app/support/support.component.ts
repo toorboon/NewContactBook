d54f27c4b16caa8cfd70a883aab16ee6721308ca
Nabil Serri Charif <Nabeel.serri@gmail.com>
2018-10-23 17:08:45 +0200

    support

--- Project/Project/src/app/support/support.component.ts
+++ Project/Project/src/app/support/support.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./support.component.sass']
 })
 export class SupportComponent implements OnInit {
-
+name: string;
+email: string;
+message: string;
   constructor() { }
 
   ngOnInit() {
   }
-
+processForm() {
+    const allInfo = `My name is ${this.name}. My email is ${this.email}. My message is ${this.message}`;
+    alert(allInfo); 
+  }
 }
