6f132ae26632e51991cb5b32e137045d6f39eb32
Marco Häfner <soledat@gmx.net>
2018-10-23 11:32:55 +0200

    added login with FireBase Auth-Module to page

--- Project/Project/src/app/app.module.ts
+++ Project/Project/src/app/app.module.ts
@@ -4,19 +4,22 @@ import { ReactiveFormsModule,FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { AngularFireModule } from "angularfire2";
 import { AngularFireDatabaseModule } from "angularfire2/database";
 import { environment } from "../environments/environment";
+
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthService } from './shared/auth.service';
+
 import { ContactComponent } from './contact/contact.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
-import { ContactService } from "./shared/contact.service";
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { SupportComponent } from './support/support.component';
-
-
+import { ContactService } from "./shared/contact.service";
 
 @NgModule({
   declarations: [
@@ -33,11 +36,12 @@ import { SupportComponent } from './support/support.component';
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),// we called initializeApp function to provide connetion details
-   AngularFireDatabaseModule,// we will import the classes here too 
-   ReactiveFormsModule,
-   FormsModule
+    AngularFireDatabaseModule,// we will import the classes here too 
+    AngularFireAuthModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [ContactService],
+  providers: [ContactService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
