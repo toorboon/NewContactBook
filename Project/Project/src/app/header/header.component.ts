c5130b40a9d0986f06186becdcb005caa5ec1a6c
Nabil Serri Charif <Nabeel.serri@gmail.com>
2018-10-24 20:34:30 +0200

    responsiveity

--- Project/Project/src/app/header/header.component.ts
+++ Project/Project/src/app/header/header.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 	title:string;
+	title2:string;
 
   constructor() {
-  	this.title="Contact Book New";
+  	this.title="Contact Book";
+  	this.title2="New"
 
    }
 
