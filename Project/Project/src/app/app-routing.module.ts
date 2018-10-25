ce4cdea2fc075b369af29956d0cf42e6e169b59b
Marco Häfner <soledat@gmx.net>
2018-10-25 11:09:17 +0200

    finished inline documentation for app-routing.module.ts, app.component.ts and app.module.ts

--- Project/Project/src/app/app-routing.module.ts
+++ Project/Project/src/app/app-routing.module.ts
@@ -4,15 +4,14 @@ import { ContactComponent } from './contact/contact.component';
 import { SupportComponent } from './support/support.component';
 
 
-
+//here you find the routes we used, but keep in mind that these are all protected by the auth.service module (if you are not logged in, you don't see anything!)
 const routes: Routes = [
 {
-   path: '', component: ContactComponent
+   path: '', component: ContactComponent //we route per default to our main page (contact.component)
 },
 {
-   path: 'support', component: SupportComponent
+   path: 'support', component: SupportComponent //the email component
 },
-
 ];
 
 @NgModule({
